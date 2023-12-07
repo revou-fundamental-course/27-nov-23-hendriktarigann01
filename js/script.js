7a58b94d5188a63aac174cc208d712823a700043
hendriktarigann01 <hendriktarigan25@gmail.com>
2023-12-07 08:51:32 +0700

    bismillah bisa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// KELILING
 function hitungKeliling() {
   var SisiA = parseFloat(document.getElementById('input-sisiA').value);
   var SisiB = parseFloat(document.getElementById('input-sisiB').value);
@@ -14,4 +15,22 @@ function hitungKeliling() {
 // Fungsi untuk mereset hasil perhitungan
 function resetHasil() {
   document.getElementById('hasilKeliling').innerHTML = '';
+}
+
+// LUAS
+function hitungLuas() {
+  var alas = parseFloat(document.getElementById('input-alas').value);
+  var tinggi = parseFloat(document.getElementById('input-tinggi').value);
+
+  if (!isNaN(alas) && !isNaN(tinggi)) {
+    var luas = 0.5 * alas * tinggi;
+    document.getElementById('hasilLuas').innerHTML = 'Luas Segitiga: ' + luas;
+  } else {
+    document.getElementById('hasilLuas').innerHTML = 'Masukkan angka yang valid untuk alas dan tinggi';
+  }
+}
+
+// Fungsi untuk mereset hasil perhitungan
+function resetHasil2() {
+document.getElementById('hasilLuas').innerHTML = '';
 }
